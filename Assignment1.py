70378a53eecd492139b28f2012ef11d42b627893
MukhtarGhaleb <65927302+MukhtarGhaleb@users.noreply.github.com>
2023-06-10 10:40:55 +0300

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,57 @@
+# %% [markdown]
+# <h1> Data Science Tools and Ecosystem </h1>
+
+# %% [markdown]
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# %% [markdown]
+# **Objectives:**
+# - Python
+# - SQL
+# - Java
+# - Javascript
+# - Go
+
+# %% [markdown]
+# Some of the popular languages that Data Scientists use are:
+# 1. R
+# 2. Julia
+# 3. Scala
+# 4. Python
+# 5. C++
+
+# %% [markdown]
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. TensorFlow.
+# 2. NumPy.
+# 3. SciPy.
+# 4. Pandas.
+# 5. Matplotlib.
+# 6. Keras.
+
+# %% [markdown]
+# | Data Science Tools |
+# |--------------------|
+# |RStudio|
+# |Apache Spark|
+# |Apache Hadoop|
+# |RapidMiner|
+
+# %% [markdown]
+# <h3> Below are a few examples of evaluating arithmetic expressions in Python </h3>
+
+# %%
+#This a simple arithmetic expression to mutiply then add integers
+result = (3*4)+5
+print(result)
+
+# %%
+#This will convert 200 minutes to hours by diving by 60
+min = 200
+print("Hours : Minutes ", int(min/60), " : ", min % 60)
+
+# %% [markdown]
+# <h2> Author </h2>
+# Mukhtar Ghaleb
+
+
